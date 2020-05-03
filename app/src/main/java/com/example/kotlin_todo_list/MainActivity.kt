9707c8ae78b72c623689eceaeb4a9fa86dc57d51
c5apple <c5apple_party@yahoo.co.jp>
2020-05-03 12:25:31 +0900

    RecycleView

--- app/src/main/java/com/example/kotlin_todo_list/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_todo_list/MainActivity.kt
@@ -2,11 +2,42 @@ package com.example.kotlin_todo_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecyclerViewHolder.ItemClickListener {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val hoges = resources.getStringArray(R.array.hoges).toMutableList()
+        val hoges: List<String> = mutableListOf("a", "b", "c")
+
+        viewAdapter = RecyclerAdapter(this, this, hoges)
+        viewManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        recyclerView = findViewById<RecyclerView>(R.id.mainRecyclerView).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
+    }
+
+    override fun onItemClick(view: View, position: Int) {
+        Toast.makeText(applicationContext, "position $position was tapped", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/example/kotlin_todo_list/RecyclerViewHolder.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin_todo_list
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    // 独自に作成したListener
+    interface ItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    val itemTextView: TextView = view.findViewById(R.id.itemTextView)
+    val itemImageView: ImageView = view.findViewById(R.id.itemImageView)
+
+    init {
+        // layoutの初期設定するときはココ
+    }
+
+}
